743ad7cf7a6150e2f88a83d4403869465eb4d0f8
Umbrella <289787539@qq.com>
2025-07-24 16:40:11 +0800

    ce

---
+++ .history/vite.config_20250724163715.ts
@@ -0,0 +1,31 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(
+        new URL(
+          // 如果是生产环境则为"./src/",否则为"./"
+          import.meta.env.MODE == "production" ? "./aa" : "./src",
+          import.meta.url
+        )
+      ),
+    },
+  },
+  base: "./",
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://www.vinevine.shop/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
