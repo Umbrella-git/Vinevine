8009a1428455693f952df633b2f46092199185d4
Umbrella <289787539@qq.com>
2025-07-24 18:33:30 +0800

    ce

---
+++ .history/src/util/utils_20250724183154.ts
@@ -0,0 +1,6 @@
+// 获取assets静态资源
+export const getAssetsFile = (url: string) => {
+  console.log(url);
+  console.log(new URL(`../assets/img/main_img/` + url, import.meta.url).href);
+  return new URL(`../assets/img/main_img/` + url, import.meta.url).href;
+};
