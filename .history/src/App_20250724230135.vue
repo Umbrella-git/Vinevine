b886d62e9a3ee18db0db8db2e55b99148260f495
Umbrella <289787539@qq.com>
2025-07-24 23:02:20 +0800

    冗余删减

--- .history/src/App_20250724230135.vue
+++ .history/src/App_20250724230135.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
-
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
 </script>
-
+1
 <template>
   <el-header><Header /></el-header>
   <el-main><RouterView /></el-main>
